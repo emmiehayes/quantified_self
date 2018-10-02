652be5ed901a9325b2b7fb5a3ba00474630e2a87
Emmie Hayes <emmiehayes2@gmail.com>
2018-10-01 18:01:40 -0600

    remove response from swagger if not handling

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -5,17 +5,12 @@ class Api::V1::FoodsController < ApplicationController
   swagger_api :index do
     summary 'Fetch all foods'
     response :ok
-    response :unauthorized
-    response :not_acceptable
-    response :not_found
   end
 
   swagger_api :show do
     summary 'Fetch a single food by passing the food id'
     param :path, :id, :integer, :required, "Food Id"
     response :ok
-    response :unauthorized
-    response :not_acceptable
     response :not_found
   end
 
