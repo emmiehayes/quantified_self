a235c5044dec5fef1bcc6a39c777ecc69f1ed0ad
Emmie Hayes <emmiehayes2@gmail.com>
2018-10-03 16:09:10 -0600

    Add route for meal show

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       namespace :v1 do
 
       resources :foods, only: [:index, :show, :create, :update, :destroy]
-      resources :meals, only: [:index]
+      resources :meals, only: [:index, :show]
     end
   end
 end
