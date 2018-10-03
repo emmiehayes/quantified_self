71cbb9861148f15ae5061a83d1b212c54e416707
Emmie Hayes <emmiehayes2@gmail.com>
2018-10-03 16:08:27 -0600

    Write request specs for meal show happy and sad paths

--- spec/requests/meal_request_spec.rb
+++ spec/requests/meal_request_spec.rb
@@ -18,4 +18,35 @@ describe "Meals API" do
       expect(meal[:foods]).to eq([{:id=>1, :name=>"MyString", :calories=>1}, {:id=>2, :name=>"MyString", :calories=>1}])
     end
   end
+
+  context "GET /api/v1/meals/:id" do
+    it "successfully returns single meal with foods" do
+      2.times do 
+        create(:meal, :with_foods)
+      end
+    
+      meal = Meal.first
+
+      get "/api/v1/meals/#{meal.id}.json"
+      expect(response).to be_successful
+
+      meal_1 = JSON.parse(response.body, symbolize_names: true)
+
+      expect(meal_1[:id]).to eq(meal.id)
+      expect(meal_1).to have_key(:name)
+      expect(meal_1).to have_key(:foods)
+    end
+
+    it "returns 404 with message when id requested does not exist" do
+      non_existing_meal_id = 1000
+
+      get "/api/v1/meals/#{non_existing_meal_id}.json"
+      expect(response).to have_http_status(404)
+
+      result = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(result).to have_key(:message)
+      expect(result[:message]).to eq("Couldn't find Meal with 'id'=1000")
+    end
+  end
 end
