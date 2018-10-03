3718cc11cc3ec41eec1707514ab4b806de221f2b
Emmie Hayes <emmiehayes2@gmail.com>
2018-10-03 16:09:46 -0600

    Add show method and swagger to meal controller, all test passing

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::MealsController < ApplicationController
+  before_action :set_meal, only:[:show]
 
   swagger_controller :meals, 'Meals'
 
@@ -7,7 +8,24 @@ class Api::V1::MealsController < ApplicationController
     response :ok
   end
 
+   swagger_api :show do
+    summary 'Fetch a single meal by passing the meal id'
+    param :path, :id, :integer, :required, "Meal Id"
+    response :ok
+    response :not_found
+  end
+
   def index
     render json: Meal.all
+  end
+
+  def show
+    render json: @meal
+  end
+
+  private 
+
+  def set_meal
+    @meal = Meal.find(params[:id])
   end
 end
