0bb9e05715c745e254c4aececa5e0db269ad2bc3
Emmie Hayes <emmiehayes2@gmail.com>
2018-10-03 14:06:53 -0600

    Write model specs for meal and mealfood models

--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Food, type: :model do
+RSpec.describe Meal, type: :model do
   context "validations" do
     it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:calories) }
   end
 
   context "relationships" do
     it { should have_many(:meal_foods)}
-    it { should have_many(:meals)}
+    it { should have_many(:foods)}
   end
 end
