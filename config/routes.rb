017bf70604886e25a3e583b9d7d34142c91b5d9d
Emmie Hayes <emmiehayes2@gmail.com>
2018-10-03 18:32:49 -0600

    Update routes to handle post new food to meal

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
       get '/meals/:meal_id/foods', to: 'meals#show'
+      post '/meals/:meal_id/foods/:food_id', to: 'meal_foods#create'
+
+
       resources :foods, only: [:index, :show, :create, :update, :destroy]
       resources :meals, only: [:index]
     end
