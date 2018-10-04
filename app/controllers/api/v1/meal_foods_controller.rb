048f51443dfd9f31e0c208d77d6a6613538dc936
Emmie Hayes <emmiehayes2@gmail.com>
2018-10-03 19:40:44 -0600

    Update controller to handle destroy functionality

--- app/controllers/api/v1/meal_foods_controller.rb
+++ app/controllers/api/v1/meal_foods_controller.rb
@@ -2,7 +2,14 @@ class Api::V1::MealFoodsController < ApplicationController
 
   def create
     meal_food = MealFood.create!(meal_food_params)
-    render json: meal_food
+    render json: meal_food, scope: { action: "create" }
+  end
+
+  def destroy
+    meal_food = MealFood.where('meal_id = ? AND food_id = ?', meal_food_params[:meal_id], meal_food_params[:food_id]).first
+    if meal_food.destroy
+      render json: meal_food, scope: { action: "remove" }
+    end
   end
 
   private
