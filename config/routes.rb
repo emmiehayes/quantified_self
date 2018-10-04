4c9e83ea13f79ae46502d6709698a023a42b4f4c
Emmie Hayes <emmiehayes2@gmail.com>
2018-10-03 19:40:02 -0600

    add custom route to handle delete food from meal

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       namespace :v1 do
       get '/meals/:meal_id/foods', to: 'meals#show'
       post '/meals/:meal_id/foods/:food_id', to: 'meal_foods#create'
+      delete '/meals/:meal_id/foods/:food_id', to: 'meal_foods#destroy'
 
 
       resources :foods, only: [:index, :show, :create, :update, :destroy]
