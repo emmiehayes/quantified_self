b1d699691d7fc2195b404606872800f983230287
Emmie Hayes <emmiehayes2@gmail.com>
2018-10-06 17:41:42 -0600

    add counter cache spec on model level

--- spec/models/meal_food_spec.rb
+++ spec/models/meal_food_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 RSpec.describe MealFood, type: :model do
   it { should belong_to :meal }
-  it { should belong_to :food }
+  it { should belong_to(:food).counter_cache(:meals_count) }
 end
