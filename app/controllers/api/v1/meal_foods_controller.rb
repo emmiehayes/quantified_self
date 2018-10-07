beab71b8b74ff34e1175eb219e9d351a34256d7c
Emmie Hayes <emmiehayes2@gmail.com>
2018-10-06 18:02:25 -0600

    Move logic from controller to model for mealfoods

--- app/controllers/api/v1/meal_foods_controller.rb
+++ app/controllers/api/v1/meal_foods_controller.rb
@@ -6,10 +6,8 @@ class Api::V1::MealFoodsController < ApplicationController
   end
 
   def destroy
-    meal_food = MealFood.where('meal_id = ? AND food_id = ?', meal_food_params[:meal_id], meal_food_params[:food_id]).first
-    if meal_food.destroy
-      render json: meal_food, scope: { action: "remove" }
-    end
+    meal_food = MealFood.squash_connection(meal_food_params[:meal_id], meal_food_params[:food_id])
+    render json: meal_food, scope: { action: "remove" } if meal_food.destroy
   end
 
   private
